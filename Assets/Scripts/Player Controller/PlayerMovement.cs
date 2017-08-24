87220605cc257561907054de7e4db8e611c0ada4
Jake Perry <jake-perry@live.com>
2017-08-24 15:18:29 +0930

    Started creation of custom 3rd Person controller
    Started creating a custom Third Person Controller which will be useable by the player and AI characters.
    PlayerMovement takes player input & sends it to the ThirdPersonController component.

---
+++ Assets/Scripts/Player Controller/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ThirdPersonController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    ThirdPersonController _tpc = null;
+
+    void Awake()
+    {
+        _tpc = GetComponent<ThirdPersonController>();
+    }
+
+    void FixedUpdate()
+    {
+        // Get input
+        float horz = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = Vector3.zero;
+        Camera cam = Camera.main;
+        if (cam != null)
+        {
+            Vector3 forward = Vector3.Scale(cam.transform.forward, new Vector3(1, 0, 1)).normalized;
+            move = vert * forward + horz * cam.transform.right;
+
+            _tpc.Move(move);
+        }
+    }
+}
---
+++ Assets/Scripts/Third Person Controller/ThirdPersonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Animator))]
+public class ThirdPersonController : MonoBehaviour
+{
+    #region References
+
+    private Rigidbody _rb = null;
+    private Animator _animator = null;
+    private CapsuleCollider _col = null;
+
+    #endregion
+
+    #region Default Values
+
+    private float _originalGroudCheckDist = 0.0f;
+    private float _colHeight = 0.0f;
+    private Vector3 _colCenter = Vector3.zero;
+
+    #endregion
+
+    #region Inspector Variables
+
+    [Range(0.05f, 1.5f), SerializeField]    private float _groundCheckDist = 0.1f;
+
+    #endregion
+
+
+    private void Awake()
+    {
+        // Get references
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        _col = GetComponent<CapsuleCollider>();
+
+        // Store default values
+        _colHeight = _col.height;
+        _colCenter = _col.center;
+        _originalGroudCheckDist = _groundCheckDist;
+
+        // Constrain rigidbody
+        _rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    public void Move(Vector3 direction)
+    {
+        if (direction.magnitude <= 0)
+            return;
+        if (direction.magnitude != 1.0f)
+            direction.Normalize();
+
+        // TODO
+    }
+}
