6848d3d2589ca445e951df819ab47fc0ed684832
Jake Perry <jake-perry@live.com>
2017-08-25 16:35:11 +0930

    Audio System options sliders & saving values
    Added an AudioSliders script which can be used to designate UI sliders for audio channel values.
    AudioSourceManager's SetChannelValue function now saves values after modifications are made.

---
+++ Assets/Scripts/Audio System/AudioSliders.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSliders : MonoBehaviour
+{
+    #region Sliders
+
+    [SerializeField]    private Slider _masterSlider = null;
+    [SerializeField]    private Slider _effectsSlider = null;
+
+    #endregion
+
+    #region Shorthand Const Definitions
+
+    private const AudioSourceManager.AudioChannel MASTER = AudioSourceManager.AudioChannel.Master;
+    private const AudioSourceManager.AudioChannel EFFECTS = AudioSourceManager.AudioChannel.Effects;
+
+    #endregion
+
+    void Awake()
+    {
+        if (_masterSlider != null)
+            _masterSlider.onValueChanged.AddListener(delegate 
+            { valueChanged(MASTER); }
+            );
+
+        if (_effectsSlider != null)
+            _effectsSlider.onValueChanged.AddListener(delegate
+            { valueChanged(EFFECTS); }
+            );
+    }
+
+    private void valueChanged(AudioSourceManager.AudioChannel channel)
+    {
+        AudioSourceManager manager = AudioSourceManager.instance;
+
+        if (manager != null)
+        {
+            switch (channel)
+            {
+                case MASTER:
+                    {
+                        if (_masterSlider != null)
+                            manager.SetChannelValue(MASTER, _masterSlider.normalizedValue);
+                        break;
+                    }
+
+                case EFFECTS:
+                    {
+                        if (_effectsSlider != null)
+                            manager.SetChannelValue(EFFECTS, _effectsSlider.normalizedValue);
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+        }
+    }
+}
