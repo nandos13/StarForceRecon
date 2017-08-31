2a56694ceca0608a630fa3248d321ffbbe3aeaf8
Damian <damian.castle@hotmail.com>
2017-08-31 14:22:25 +0930

    Added functionally of doors
    Added working door open animator script.
    adjusted animations for door opening and close

---
+++ Assets/Scripts/Misc/DoorOpenAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenAnimator : MonoBehaviour {
+
+     
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<Animator>().SetBool("DoorOpen", true);
+          
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // do we have any players left inside?
+            bool playersInside = false;
+            BoxCollider box = GetComponent<BoxCollider>();
+            Collider[] cols = Physics.OverlapBox(box.bounds.center, box.bounds.extents, box.transform.rotation);
+            foreach (Collider c in cols)
+                if (c.tag == "Player")
+                    playersInside = true;
+
+            if (playersInside == false)
+                gameObject.GetComponent<Animator>().SetBool("DoorOpen", false);
+          
+        }
+    }
+}
