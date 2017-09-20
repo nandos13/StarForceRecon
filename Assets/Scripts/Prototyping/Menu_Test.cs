5d2129890160a41750024ef9929ea91c37027ac2
Unknown <kalebismadman@gmail.com>
2017-09-20 16:30:12 +0930

    Menu_test
    Began making menu test scene, made test animations, and test script

---
+++ Assets/Scripts/Prototyping/Menu_Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_Test : MonoBehaviour {
+
+    Animator _animator;
+    bool settingMenu = false;
+    bool pressed = false;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void AnyKey()
+    {
+        if(pressed == false)
+        {
+            
+        }
+    }
+
+}
