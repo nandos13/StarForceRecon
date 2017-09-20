9e7b093fecf81bc62346fce160279e8f5c5a57a5
Unknown <kalebismadman@gmail.com>
2017-09-20 20:25:47 +0930

    Worked on menu_test
    Its completed in the idea, but needs work still

--- Assets/Scripts/Prototyping/Menu_Test.cs
+++ Assets/Scripts/Prototyping/Menu_Test.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class Menu_Test : MonoBehaviour {
 
     Animator _animator;
-    bool settingMenu = false;
+    bool _settings = false;
+    bool _exit = false;
     bool pressed = false;
 
     void Awake()
@@ -14,12 +15,62 @@ public class Menu_Test : MonoBehaviour {
         _animator = GetComponent<Animator>();
     }
 
-    public void AnyKey()
+    void Update()
     {
-        if(pressed == false)
+        if(Input.GetButtonDown("Fire1"))
         {
-            
+            _animator.SetBool("AnyKey", true);
         }
+    }
+
+    public void Settings()
+    {
+        if (pressed == false)
+        {
+            if (_settings == false)
+            {
+                pressed = true;
+                Invoke("Reset", 1f);
+                _settings = true;
+                _animator.SetBool("Settings", true);
+            }
+
+            else if (_settings == true)
+            {
+                pressed = true;
+                Invoke("Reset", 1f);
+                _settings = false;
+                _animator.SetBool("Settings", false);
+            }
+        }
+    }
+
+    public void Exit()
+    {
+        if (pressed == false)
+        {
+            if (_exit == false)
+            {
+                pressed = true;
+                Invoke("Reset", 1f);
+                _exit = true;
+                _animator.SetBool("Exit", true);
+            }
+
+            else if (_exit == true)
+            {
+                pressed = true;
+                Invoke("Reset", 1f);
+                _exit = false;
+                _animator.SetBool("Exit", false);
+            }
+        }
+    }
+
+
+    void Reset()
+    {
+        pressed = false;
     }
 
 }
