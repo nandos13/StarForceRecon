ea7e331bbb625cfdbeea75628bdaebba5cf97349
Jake Perry <jake-perry@live.com>
2017-10-13 13:22:45 +1030

    Squad Follow, Prefab model loading
    Added SquadFollow script for basic squad grouping. More work required to make this behave nicely.
    Changed PlayerController to have a list of Behaviour rather than MonoBehaviour, for both AI and Player scripts. This allows things like NavMeshAgent to be toggled with character swapping.
    Added InitializeChildResource script which aims to instantiate an object in Resources as a child of the transform. This is now used to add the mesh to the player character prefabs, which means only one prefab is needed for all characters.

---
+++ Assets/Scripts/UAI Behaviours/SquadFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+using StarForceRecon;
+
+public class SquadFollow : MonoBehaviour
+{
+    #region Locator
+
+    /// <summary>Aims to keep squad members spaced out around the player without mobbing behind.</summary>
+    public static class Locator
+    {
+        // TODO
+    }
+
+    #endregion
+
+    #region Variables
+
+    private NavMeshAgent navAgent = null;
+    private ThirdPersonController tpc = null;
+
+    #endregion
+
+    private void Awake()
+    {
+        navAgent = GetComponentInParent<NavMeshAgent>();
+        if (navAgent == null)
+            throw new System.Exception(string.Format("{0} script requires a NavMeshAgent component within the parent hierarchy.", this.GetType()));
+
+        tpc = GetComponentInParent<ThirdPersonController>();
+    }
+
+    private void Update()
+    {
+        SquadManager.IControllable player = SquadManager.GetCurrentSquaddie;
+
+        if (player != null)
+            navAgent.SetDestination(player.transform.position);
+
+        if (navAgent.remainingDistance > navAgent.stoppingDistance)
+            tpc.Move(navAgent.desiredVelocity, false, false);
+        else
+            tpc.Move(Vector3.zero, false, false);
+    }
+}
