51d5b8283b682e14299a3f15c749c5ca09d613ff
Unknown <kalebismadman@gmail.com>
2017-10-19 16:00:29 +1030

    Creating Pause
    Creating pause function and a hud.

---
+++ Assets/Scripts/Prototyping/K_Pause.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class K_Pause : MonoBehaviour {
+
+    public GameObject _hudCanvas;
+    public GameObject _pauseCanvas;
+
+    bool _paused;
+
+    void Start()
+    {
+        _paused = false;
+    }
+
+    void Update()
+    {
+        if (_paused == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _hudCanvas.SetActive(false);
+                Time.timeScale = 0.001f;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                _pauseCanvas.SetActive(true);
+                _paused = true;
+            }
+        }
+
+        else if (_paused == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _pauseCanvas.SetActive(false);
+                Time.timeScale = 1.0f;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                _hudCanvas.SetActive(true);
+                _paused = false;
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        if (_paused == true) {
+            _pauseCanvas.SetActive(false);
+            Time.timeScale = 1.0f;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            _hudCanvas.SetActive(true);
+            _paused = false;
+        }
+    }
+}
