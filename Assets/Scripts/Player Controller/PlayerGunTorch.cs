b3e261cc3690bc8b6bf3668304ddc17734394bf1
Jake Perry <jake-perry@live.com>
2017-10-25 15:31:14 +1030

    Added toggleable flashlight
    Added a flashlight script which allows a button to be pressed to toggle a light.

---
+++ Assets/Scripts/Player Controller/PlayerGunTorch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JakePerry;
+
+namespace StarForceRecon
+{
+    public class PlayerGunTorch : MonoBehaviour, GameController<SFRInputSet>.ITarget
+    {
+        [SerializeField]
+        private Light flashlight;
+
+        private void Awake()
+        {
+            GameController<SFRInputSet> kbm = new KeyboardMouseController<SFRInputSet>(this);
+            GameController<SFRInputSet> controller = new DualStickController<SFRInputSet>(this);
+        }
+
+        void GameController<SFRInputSet>.ITarget.ReceiveControllerInput(SFRInputSet inputSet, GameController<SFRInputSet>.ControlType controllerType)
+        {
+            if (flashlight != null && inputSet.GetActionByName("flashlight").WasPressed)
+                flashlight.enabled = !flashlight.enabled;
+        }
+    }
+}
