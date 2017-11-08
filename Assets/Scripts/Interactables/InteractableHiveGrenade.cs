29ae2e7fd5a9063900ea83fcfd930b0b8b77a036
Jake Perry <jake-perry@live.com>
2017-11-08 10:43:06 +1030

    Explosion interactable
    Finished explosion interactable. Is now able to destroy spawn nodes and play a particle.

--- Assets/Scripts/Interactables/InteractableHiveGrenade.cs
+++ Assets/Scripts/Interactables/InteractableHiveGrenade.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableDestructable : MonoBehaviour, Interaction.IInteractable
+public class InteractableHiveGrenade : MonoBehaviour, Interaction.IInteractable
 {
     #region Variables
     [Tooltip("If true, interacting character will become unuseable while the interaction is happening.")]
     [SerializeField]
     private bool lockCharacter = true;
 
-    [Tooltip("Duration in seconds the interaction will take to complete.")]
+    [Tooltip("Duration in seconds to drop the grenade.")]
     [SerializeField, Range(0.0f, 120.0f)]
-    private float duration = 0.0f;
-    
+    private float placementDuration = 0.0f;
+
+    [Tooltip("Time in seconds before the grenade will explode.")]
+    [SerializeField, Range(1.0f, 10.0f)]
+    private float grenadeFuseTime = 3.0f;
+
+    [SerializeField]
+    private SpawnNode nodeToDestroy = null;
+
+    [SerializeField]
+    private ParticleSystem explosion = null;
+
+    [SerializeField, Range(0.1f, 5.0f)]
+    private float particleLifeTime = 1.0f;
+
     [SerializeField]
     private Vector3 focusPointOffset = Vector3.zero;
 
@@ -25,8 +38,14 @@ public class InteractableDestructable : MonoBehaviour, Interaction.IInteractable
     private GameObject enablePiece;
     #endregion
 
+    private void Awake()
+    {
+        if (explosion != null)
+            explosion.gameObject.SetActive(false);
+    }
+
     #region IInteractable Methods
-    float Interaction.IInteractable.Duration { get { return duration; } }
+    float Interaction.IInteractable.Duration { get { return placementDuration; } }
 
     Vector3 Interaction.IInteractable.FocusPoint { get { return transform.TransformPoint(focusPointOffset); } }
 
@@ -36,11 +55,38 @@ public class InteractableDestructable : MonoBehaviour, Interaction.IInteractable
 
     void Interaction.IInteractable.OnCompleteInteraction()
     {
+        StartCoroutine(GrenadeExplosionTimer(grenadeFuseTime));
+
+        Animator animator = GetComponentInParent<Animator>();
+        if (animator != null)
+            animator.SetTrigger("GrenadeInteraction");
+    }
+
+    private IEnumerator GrenadeExplosionTimer(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        if (nodeToDestroy != null)
+            Destroy(nodeToDestroy);
+
+        if (explosion != null)
+        {
+            explosion.gameObject.SetActive(true);
+            explosion.Play();
+            StartCoroutine(ExplosionLifetime(particleLifeTime));
+        }
+
         if (disablePiece)
             disablePiece.SetActive(false);
 
         if (enablePiece)
             enablePiece.SetActive(true);
+    }
+
+    private IEnumerator ExplosionLifetime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        explosion.gameObject.SetActive(false);
     }
 
     void Interaction.IInteractable.OnStartInteraction(Interaction.InteractionInfo info)
