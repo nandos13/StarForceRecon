abc7b3597987b7b9bbedb728846b86f2322efc9d
Unknown <kalebismadman@gmail.com>
2017-11-09 16:35:57 +1030

    Working on test Scene
    Added bens character material, working on lighting in level 4, adding sound.

---
+++ Assets/Scripts/Prototyping/lvl4light.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvl4light : MonoBehaviour {
+
+    public GameObject lights1, lights2, lights3;
+
+    private IEnumerator coroutine;
+    bool _active;
+
+    void Start()
+    {
+        coroutine = WaitAndPrint(1f);
+        _active = true;
+    }
+
+    void OnTriggerEnter(Collider c) {
+        if (c.gameObject.tag == "Player" && _active == true)
+        {
+            StartCoroutine(coroutine);
+            _active = false;
+        }
+    }
+    private IEnumerator WaitAndPrint(float waitTime = 3500f)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            lights1.SetActive(true);
+            yield return new WaitForSeconds(waitTime*2);
+            lights2.SetActive(true);
+            yield return new WaitForSeconds(waitTime*3);
+            lights3.SetActive(true);
+        }
+    }
+}
