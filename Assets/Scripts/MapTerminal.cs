fc19320374b43f5c8667b5668e0311014e26b8e9
Brett Madigan <TheRAMPAGE572@hotmail.com>
2017-11-16 14:02:16 +1030

    Updated Map Terminal prefab, Added map pop ups to the canvas.

---
+++ Assets/Scripts/MapTerminal.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTerminal : MonoBehaviour
+{
+
+    public GameObject mapTerm;
+    bool _paused;
+    bool canShowMap = false;
+
+    void OnTriggerEnter()
+    {
+        canShowMap = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        canShowMap = false;
+    }
+
+    void Update()
+    {
+        if (canShowMap)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (mapTerm.activeSelf)
+                {
+                    mapTerm.SetActive(false);
+                    Time.timeScale = 1;
+                }
+                else
+                {
+                    mapTerm.SetActive(true);
+                    Time.timeScale = 0;
+                }
+            }
+        }
+    }
+}
+
+//    void Update()
+//    {
+//        if (mapTerm == false && !_paused)
+//        {
+//            if (Input.GetKeyDown(KeyCode.F))
+//            {
+//                SetPauseState(true);
+//            }
+//        }
+
+//        else if (mapTerm == true && _paused)
+//        {
+//            if (Input.GetKeyDown(KeyCode.F))
+//            {
+//                SetPauseState(false);
+//            }
+//        }
+//    }
+
+//    private void SetPauseState(bool state)
+//    {
+//        mapTerm.SetActive(!state);
+//        Time.timeScale = state ? 0.0f : 1.0f;
+//        Cursor.visible = state;
+//        if (state)
+//            Cursor.lockState = CursorLockMode.None;
+//        mapTerm.SetActive(state);
+//        _paused = state;
+//    }
+//}
