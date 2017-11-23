2475dfe48608db37f7e3424ff5f41c67cbcfab96
Unknown <kalebismadman@gmail.com>
2017-11-23 12:18:41 +1030

    Working on dialogue scene

---
+++ Assets/Scripts/Prototyping/DialogueTracker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTracker : MonoBehaviour {
+
+    public GameObject _dialogue;
+    public GameObject _skip;
+
+	// Use this for initialization
+	void Awake () {
+        _dialogue.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Prototyping/TextType.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextType : MonoBehaviour {
+
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    IEnumerator ShowText()
+    {
+        for(int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0,i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
