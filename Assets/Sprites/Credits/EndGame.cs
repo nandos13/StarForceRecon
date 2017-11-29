d5e5c6673222855f4cb37d497abd9d45c0e429b5
Brett Madigan <TheRAMPAGE572@hotmail.com>
2017-11-29 13:19:35 +1030

    Added song to credits scene and added engame script
    Loads menu when credits scroll through

---
+++ Assets/Sprites/Credits/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour {
+
+    IEnumerator WaitandLoad()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(17);
+        SceneManager.LoadScene("Menu");
+        //print("WaitAndPrint " + Time.time);
+    }
+
+    IEnumerator Start()
+    {
+        //print("Starting " + Time.time);
+
+        // Start function WaitAndPrint as a coroutine
+        yield return StartCoroutine("WaitandLoad");
+        //print("Done " + Time.time);
+    }
+}
